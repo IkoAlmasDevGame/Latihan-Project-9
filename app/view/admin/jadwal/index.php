d31a7105a8c71ce85cddb23279f0ce8fdaadee5e
IkoAlmasDev <ikoalmas27@gmail.com>
2024-03-17 17:02:54 +0700

    update jadwal

---
+++ app/view/admin/jadwal/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Halaman Jadwal</title>
+        <?php 
+            require_once("../ui/header.php");
+        ?>
+    </head>
+
+    <body>
+        <?php 
+            require_once("../ui/navbar.php");
+        ?>
+        <div class="container-fluid py-5 p-5 bg-secondary rounded-1 min-vh-100">
+            <div class="container-fluid bg-light rounded-1 min-vh-100">
+
+            </div>
+        </div>
+        <?php 
+            require_once("../ui/footer.php");
+        ?>
