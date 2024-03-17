47289b39de34ed389038ccc80c96f264790745dd
IkoAlmasDev <ikoalmas27@gmail.com>
2024-03-17 17:04:36 +0700

    update footer ui admin

--- app/view/admin/ui/footer.php
+++ app/view/admin/ui/footer.php
@@ -9,6 +9,27 @@ $(document).ready(function() {
     $("#example1").DataTable();
     $("#example2").DataTable();
 });
+
+function startTime() {
+    var day = ["Ahad", "Itsnain", "tsulatsa", "Arbia", "Khamiis ", "Jumu’ah", "Sabtu"];
+    var today = new Date();
+    var tahun = today.getFullYear();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML =
+        day[today.getDay()] + ", " + h + ":" + m + ":" + s + ", " + tahun;
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i
+    }; // add zero in front of numbers < 10
+    return i;
+}
 </script>
 </body>
 
