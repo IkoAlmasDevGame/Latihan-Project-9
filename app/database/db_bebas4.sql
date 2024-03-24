9c0f776dc12cdaa084940504e1c11b36cdff6cf8
IkoAlmasDev <ikoalmas27@gmail.com>
2024-03-24 15:32:35 +0700

    update database sql

---
+++ app/database/db_bebas4.sql
@@ -0,0 +1,315 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 24, 2024 at 09:27 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_bebas4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_absensi`
+--
+
+CREATE TABLE `tb_absensi` (
+  `id_absensi` int(11) NOT NULL,
+  `id_siswa` int(11) NOT NULL,
+  `id_kelas` int(3) NOT NULL,
+  `keterangan` enum('h','s','i','a') NOT NULL,
+  `tanggal` varchar(12) NOT NULL,
+  `selesai` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_guru`
+--
+
+CREATE TABLE `tb_guru` (
+  `id_guru` int(11) NOT NULL,
+  `nip` int(18) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `tanggal_lahir` varchar(12) NOT NULL,
+  `jeniskelamin` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_jadwal`
+--
+
+CREATE TABLE `tb_jadwal` (
+  `id_jadwal` int(11) NOT NULL,
+  `id_jam` int(11) DEFAULT NULL,
+  `id_kelas` int(11) DEFAULT NULL,
+  `id_pelajaran` int(11) DEFAULT NULL,
+  `id_guru` int(11) DEFAULT NULL,
+  `hari` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_jam`
+--
+
+CREATE TABLE `tb_jam` (
+  `id_jam` int(11) NOT NULL,
+  `jam` int(11) NOT NULL,
+  `mulai` varchar(30) NOT NULL,
+  `akhir` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kelas`
+--
+
+CREATE TABLE `tb_kelas` (
+  `id_kelas` int(11) NOT NULL,
+  `namakelas` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pelajaran`
+--
+
+CREATE TABLE `tb_pelajaran` (
+  `id_pelajaran` int(11) NOT NULL,
+  `pelajaran` varchar(128) NOT NULL,
+  `create_timestamp` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pendaftaran`
+--
+
+CREATE TABLE `tb_pendaftaran` (
+  `id_siswa` int(11) NOT NULL,
+  `nis` int(10) NOT NULL,
+  `nama_lengkap` varchar(128) NOT NULL,
+  `tempat_lahir` varchar(128) NOT NULL,
+  `tanggal_lahir` int(32) NOT NULL,
+  `agama` varchar(32) NOT NULL,
+  `jumlah_saudara` int(11) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `nama_ayah` varchar(128) NOT NULL,
+  `pekerjaan_ayah` varchar(100) NOT NULL,
+  `telepon_ayah` int(13) NOT NULL,
+  `nama_ibu` varchar(128) NOT NULL,
+  `pekerjaan_ibu` varchar(100) NOT NULL,
+  `telepon_ibu` int(13) NOT NULL,
+  `file_kk` varchar(200) NOT NULL,
+  `file_akte` varchar(200) NOT NULL,
+  `file_image` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pesan`
+--
+
+CREATE TABLE `tb_pesan` (
+  `id_pesan` int(11) NOT NULL,
+  `toFrom` varchar(255) NOT NULL,
+  `toTo` varchar(255) NOT NULL,
+  `toSubject` varchar(255) NOT NULL,
+  `toMessage` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_siswa`
+--
+
+CREATE TABLE `tb_siswa` (
+  `kd_siswa` int(11) NOT NULL,
+  `id_siswa` int(11) NOT NULL,
+  `id_kelas` int(3) NOT NULL,
+  `selesai` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id_user` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `user_level` varchar(32) NOT NULL,
+  `created_At` timestamp NOT NULL DEFAULT current_timestamp(),
+  `created_End` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_absensi`
+--
+ALTER TABLE `tb_absensi`
+  ADD PRIMARY KEY (`id_absensi`),
+  ADD KEY `id_siswa` (`id_siswa`),
+  ADD KEY `id_kelas` (`id_kelas`);
+
+--
+-- Indexes for table `tb_guru`
+--
+ALTER TABLE `tb_guru`
+  ADD PRIMARY KEY (`id_guru`);
+
+--
+-- Indexes for table `tb_jadwal`
+--
+ALTER TABLE `tb_jadwal`
+  ADD PRIMARY KEY (`id_jadwal`),
+  ADD KEY `id_jam` (`id_jam`),
+  ADD KEY `id_kelas` (`id_kelas`),
+  ADD KEY `id_pelajaran` (`id_pelajaran`),
+  ADD KEY `id_guru` (`id_guru`);
+
+--
+-- Indexes for table `tb_jam`
+--
+ALTER TABLE `tb_jam`
+  ADD PRIMARY KEY (`id_jam`);
+
+--
+-- Indexes for table `tb_kelas`
+--
+ALTER TABLE `tb_kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
+-- Indexes for table `tb_pelajaran`
+--
+ALTER TABLE `tb_pelajaran`
+  ADD PRIMARY KEY (`id_pelajaran`);
+
+--
+-- Indexes for table `tb_pendaftaran`
+--
+ALTER TABLE `tb_pendaftaran`
+  ADD PRIMARY KEY (`id_siswa`) USING BTREE;
+
+--
+-- Indexes for table `tb_pesan`
+--
+ALTER TABLE `tb_pesan`
+  ADD PRIMARY KEY (`id_pesan`);
+
+--
+-- Indexes for table `tb_siswa`
+--
+ALTER TABLE `tb_siswa`
+  ADD PRIMARY KEY (`kd_siswa`),
+  ADD KEY `id_siswa` (`id_siswa`),
+  ADD KEY `id_kelas` (`id_kelas`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_absensi`
+--
+ALTER TABLE `tb_absensi`
+  MODIFY `id_absensi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_guru`
+--
+ALTER TABLE `tb_guru`
+  MODIFY `id_guru` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_jadwal`
+--
+ALTER TABLE `tb_jadwal`
+  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_jam`
+--
+ALTER TABLE `tb_jam`
+  MODIFY `id_jam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tb_kelas`
+--
+ALTER TABLE `tb_kelas`
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tb_pelajaran`
+--
+ALTER TABLE `tb_pelajaran`
+  MODIFY `id_pelajaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tb_pendaftaran`
+--
+ALTER TABLE `tb_pendaftaran`
+  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_pesan`
+--
+ALTER TABLE `tb_pesan`
+  MODIFY `id_pesan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tb_siswa`
+--
+ALTER TABLE `tb_siswa`
+  MODIFY `kd_siswa` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
